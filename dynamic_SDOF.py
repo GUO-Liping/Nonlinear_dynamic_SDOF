ba289c236d0b925c6c6a11922c7ad2ddddb99910
郭立平 <liping0_0@my.swjtu.edu.cn>
2021-07-14 10:45:39 +0800

    Update dynamic_SDOF.py

--- dynamic_SDOF.py
+++ dynamic_SDOF.py
@@ -1,3 +1,6 @@
+# Ben Cheng Xu Yong Yu Qiu Jie
+#"Jie Gou Dong Li Xue -Di 2 Ban"
+#Zhong Yi Dao Li Ti. Zhe dao li ti tong shi she ji cai liao fei xian xing yu zu ni fei xian xing.
 import numpy as np
 import matplotlib.pyplot as plt
 
