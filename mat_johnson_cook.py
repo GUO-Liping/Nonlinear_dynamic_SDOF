f3143eaac4f10a24701da632f1af4e28e56b177c
郭立平 <liping0_0@my.swjtu.edu.cn>
2021-08-17 15:04:23 +0800

    Update mat_johnson_cook.py

--- mat_johnson_cook.py
+++ mat_johnson_cook.py
@@ -6,16 +6,24 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-A = 1500
-B = 1500
+A = 1570
+B = 1250
 C = 1
 epsilon_p = np.arange(0,0.072,step = 0.001)
-N = 0.5
+N = 0.48
 epsilon_star_dot = 1
 eps0 = 100
 sigma_y = (A+B*epsilon_p**N) * (1+C*np.log(epsilon_star_dot))
 sig_max = 0.06
 sig_sat = 0.05
 max_stress = np.minimum(np.minimum(A+B*epsilon_p**N, sig_max)*(1+C*np.log(epsilon_star_dot)), sig_sat)
+
+strain_eng = np.array([0,0.005,0.04984,0.07])
+stress_eng = np.array([1445,1700,1880,0])
+
+strain_log = np.array([0,0.00489,0.04721,0.06766])
+stress_ture = np.array([1457.28,1725.5,1992.8,0])
+
 plt.plot(epsilon_p, sigma_y)
+plt.plot(strain_eng, stress_eng)
 plt.show()
